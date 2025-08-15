5cb635618e9c5fa020063f0da5b9f519f063afd7
wolfsripe <jose2003figueiredo@gmail.com>
2025-08-15 12:05:17 +0100

    Add files via upload

---
+++ Debug/Core/Src/subdir.mk
@@ -0,0 +1,84 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/SX1278.c \
+../Core/Src/SX1278_hw.c \
+../Core/Src/bmp280.c \
+../Core/Src/commands.c \
+../Core/Src/freertos.c \
+../Core/Src/gpio.c \
+../Core/Src/i2c.c \
+../Core/Src/imc20608.c \
+../Core/Src/main.c \
+../Core/Src/parser.c \
+../Core/Src/spi.c \
+../Core/Src/stm32f7xx_hal_msp.c \
+../Core/Src/stm32f7xx_hal_timebase_tim.c \
+../Core/Src/stm32f7xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32f7xx.c \
+../Core/Src/tim.c \
+../Core/Src/usart.c \
+../Core/Src/utils.c 
+
+OBJS += \
+./Core/Src/SX1278.o \
+./Core/Src/SX1278_hw.o \
+./Core/Src/bmp280.o \
+./Core/Src/commands.o \
+./Core/Src/freertos.o \
+./Core/Src/gpio.o \
+./Core/Src/i2c.o \
+./Core/Src/imc20608.o \
+./Core/Src/main.o \
+./Core/Src/parser.o \
+./Core/Src/spi.o \
+./Core/Src/stm32f7xx_hal_msp.o \
+./Core/Src/stm32f7xx_hal_timebase_tim.o \
+./Core/Src/stm32f7xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32f7xx.o \
+./Core/Src/tim.o \
+./Core/Src/usart.o \
+./Core/Src/utils.o 
+
+C_DEPS += \
+./Core/Src/SX1278.d \
+./Core/Src/SX1278_hw.d \
+./Core/Src/bmp280.d \
+./Core/Src/commands.d \
+./Core/Src/freertos.d \
+./Core/Src/gpio.d \
+./Core/Src/i2c.d \
+./Core/Src/imc20608.d \
+./Core/Src/main.d \
+./Core/Src/parser.d \
+./Core/Src/spi.d \
+./Core/Src/stm32f7xx_hal_msp.d \
+./Core/Src/stm32f7xx_hal_timebase_tim.d \
+./Core/Src/stm32f7xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32f7xx.d \
+./Core/Src/tim.d \
+./Core/Src/usart.d \
+./Core/Src/utils.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F767xx -c -I../Core/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1 -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/SX1278.cyclo ./Core/Src/SX1278.d ./Core/Src/SX1278.o ./Core/Src/SX1278.su ./Core/Src/SX1278_hw.cyclo ./Core/Src/SX1278_hw.d ./Core/Src/SX1278_hw.o ./Core/Src/SX1278_hw.su ./Core/Src/bmp280.cyclo ./Core/Src/bmp280.d ./Core/Src/bmp280.o ./Core/Src/bmp280.su ./Core/Src/commands.cyclo ./Core/Src/commands.d ./Core/Src/commands.o ./Core/Src/commands.su ./Core/Src/freertos.cyclo ./Core/Src/freertos.d ./Core/Src/freertos.o ./Core/Src/freertos.su ./Core/Src/gpio.cyclo ./Core/Src/gpio.d ./Core/Src/gpio.o ./Core/Src/gpio.su ./Core/Src/i2c.cyclo ./Core/Src/i2c.d ./Core/Src/i2c.o ./Core/Src/i2c.su ./Core/Src/imc20608.cyclo ./Core/Src/imc20608.d ./Core/Src/imc20608.o ./Core/Src/imc20608.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/parser.cyclo ./Core/Src/parser.d ./Core/Src/parser.o ./Core/Src/parser.su ./Core/Src/spi.cyclo ./Core/Src/spi.d ./Core/Src/spi.o ./Core/Src/spi.su ./Core/Src/stm32f7xx_hal_msp.cyclo ./Core/Src/stm32f7xx_hal_msp.d ./Core/Src/stm32f7xx_hal_msp.o ./Core/Src/stm32f7xx_hal_msp.su ./Core/Src/stm32f7xx_hal_timebase_tim.cyclo ./Core/Src/stm32f7xx_hal_timebase_tim.d ./Core/Src/stm32f7xx_hal_timebase_tim.o ./Core/Src/stm32f7xx_hal_timebase_tim.su ./Core/Src/stm32f7xx_it.cyclo ./Core/Src/stm32f7xx_it.d ./Core/Src/stm32f7xx_it.o ./Core/Src/stm32f7xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f7xx.cyclo ./Core/Src/system_stm32f7xx.d ./Core/Src/system_stm32f7xx.o ./Core/Src/system_stm32f7xx.su ./Core/Src/tim.cyclo ./Core/Src/tim.d ./Core/Src/tim.o ./Core/Src/tim.su ./Core/Src/usart.cyclo ./Core/Src/usart.d ./Core/Src/usart.o ./Core/Src/usart.su ./Core/Src/utils.cyclo ./Core/Src/utils.d ./Core/Src/utils.o ./Core/Src/utils.su
+
+.PHONY: clean-Core-2f-Src
+
